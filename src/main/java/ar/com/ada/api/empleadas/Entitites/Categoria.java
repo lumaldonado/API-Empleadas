6a44efdabb30e8b0c5fc966a02d7a984790283d3
lumaldonado <lulimaldonado98@hotmail.com>
2021-06-22 15:11:44 -0300

    se agregaron las notaciones para persistir a la base de datos

--- src/main/java/ar/com/ada/api/empleadas/Entitites/Categoria.java
+++ src/main/java/ar/com/ada/api/empleadas/Entitites/Categoria.java
@@ -1,11 +1,27 @@
 package ar.com.ada.api.empleadas.entitites;
 import java.math.BigDecimal;
 
+import javax.persistence.*;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.*;
+
+@Entity
+@Table(name = "categoria")
 public class Categoria {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITYT)
+    @Column(name = "categoria_id")
     private Integer CategoriaId;
+
     private String  nombre;
+
+    @Column(name = "sueldo_base")
     private BigDecimal sueldoBase;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Empleada> empleadas = new ArrayList<>();
     
     public Integer getCategoriaId() {
         return CategoriaId;
--- src/main/java/ar/com/ada/api/empleadas/Entitites/Empleada.java
+++ src/main/java/ar/com/ada/api/empleadas/Entitites/Empleada.java
@@ -2,15 +2,28 @@ package ar.com.ada.api.empleadas.entitites;
 import java.math.BigDecimal;
 import java.sql.Date;
 
+import javax.persistence.*;
+@Entity
+@Table(name = "empleada")
 public class Empleada {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "empleada_id")
     private Integer empleadaId;
+
     private String nombre;
     private Integer edad;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id", referencedColumnName = "categoria_id")
     private Categoria categoria;
     private BigDecimal sueldo;
     private int estado;
+
+    @Column (name = "fecha_alta")
     private Date fechaAlta;
+    @Column (name = "fecha_baja")
     private Date fechaBaja;
 
 
